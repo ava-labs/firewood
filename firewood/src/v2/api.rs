7b9ec0c481bb65ff62f826b3fb176b5651fd8588
Ron Kuris <ron.kuris@avalabs.org>
2023-04-26 22:33:49 +0000

    Address verbal comments
    Overview:
     - Added limit to size of RangeProof
     - Added per-function argument documentation
     - Explained reason for HashKey
     - Stop using a Batch<K, V> in a RangeProof; instead use Vec<(K, V>)
       This is because RangeProofs won't contain any Delete BatchOps
     - Provide convenience method to switch from Vec<(K, V>) to BatchOp

--- firewood/src/v2/api.rs
+++ firewood/src/v2/api.rs
@@ -17,6 +17,11 @@ pub trait KeyType: AsRef<[u8]> + Send + Sync + Debug + 'static {}
 pub trait ValueType: AsRef<[u8]> + Send + Sync + Debug + 'static {}
 
 /// The type and size of a single HashKey
+/// These are 256-bit hashes that are used for a variety of reasons:
+///  - They identify a version of the datastore at a specific point
+///    in time
+///  - They are used to provide integrity at different points in a
+///    proof
 pub type HashKey = [u8; 32];
 
 /// A key/value pair operation. Only put (upsert) and delete are
@@ -30,6 +35,15 @@ pub enum BatchOp<K: KeyType, V: ValueType> {
 /// A list of operations to consist of a batch that
 /// can be proposed
 pub type Batch<K, V> = Vec<BatchOp<K, V>>;
+
+/// A convenience implementation to convert a vector of key/value
+/// pairs into a batch of insert operations
+pub fn vec_into_batch<K: KeyType, V: ValueType>(value: Vec<(K, V)>) -> Batch<K, V> {
+    value
+        .into_iter()
+        .map(|(key, value)| BatchOp::Put { key, value })
+        .collect()
+}
 
 /// Errors returned through the API
 #[derive(Debug)]
@@ -50,12 +64,12 @@ pub enum Error {
 }
 
 /// A range proof, consisting of a proof of the first key and the last key,
-/// and a vector of all key
+/// and a vector of all key/value pairs
 #[derive(Debug)]
 pub struct RangeProof<K: KeyType, V: ValueType> {
     pub first_key: Proof<V>,
     pub last_key: Proof<V>,
-    pub middle: Batch<K, V>,
+    pub middle: Vec<(K, V)>,
 }
 
 /// A proof that a single key is present
@@ -70,21 +84,37 @@ pub trait Db {
     type View: DbView;
 
     /// Get a reference to a specific view based on a hash
+    ///
+    /// # Arguments
+    ///
+    /// - `hash` - Identifies the revision for the view
     async fn revision(&self, hash: HashKey) -> Result<Weak<Self::View>, Error>;
 
     /// Get the hash of the most recently committed version
     async fn root_hash(&self) -> Result<HashKey, Error>;
 
-    /// Propose a change to the database via a batch, providing back the
-    /// hash for the proposed value
+    /// Propose a change to the database via a batch
+    ///
+    /// # Arguments
+    ///
+    /// * `hash` - the HashKey that identifies the revision
+    ///            you want to propose this batch against
+    /// * `data` - A batch consisting of [BatchOp::Put] and
+    ///            [BatchOp::Delete] operations to apply
+    ///
     async fn propose<K: KeyType, V: ValueType>(
         &mut self,
         hash: HashKey,
         data: Batch<K, V>,
     ) -> Result<HashKey, Error>;
 
-    /// Commit a specific hash, that must have been proposed against the
-    /// current root
+    /// Commit a specific hash
+    ///
+    /// # Arguments
+    ///
+    /// * `hash` - The root this commit must apply against.
+    ///            If this is not the latest commit, this
+    ///            will return [Error::IncorrectRootHash]
     async fn commit(&mut self, hash: HashKey) -> Result<(), Error>;
 }
 
@@ -92,7 +122,7 @@ pub trait Db {
 /// a Weak reference when fetching via a call to [Db::revision], as these
 /// can disappear either because they became too old, or are no longer a
 /// valid revision due to a [Db::commit].
-/// 
+///
 /// You only need a DbView if you need to read from a snapshot at a given
 /// root. Don't hold a strong reference to the DbView as it prevents older
 /// views from being cleaned up.
@@ -108,9 +138,17 @@ pub trait DbView {
     async fn single_key_proof<K: KeyType, V: ValueType>(&self, key: K) -> Result<Proof<V>, Error>;
 
     /// Obtain a range proof over a set of keys
+    ///
+    /// # Arguments
+    ///
+    /// * `first_key` - If None, start at the lowest key
+    /// * `last_key` - If None, continue to the end of the database
+    /// * `limit` - The maximum number of keys in the range proof
+    ///
     async fn range_proof<K: KeyType, V: ValueType>(
         &self,
-        first_key: K,
-        last_key: K,
+        first_key: Option<K>,
+        last_key: Option<K>,
+        limit: usize,
     ) -> Result<RangeProof<K, V>, Error>;
 }
