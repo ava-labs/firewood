de4b54c08fbc82ccdec4fd2f8bfad4bc1de74fa8
Ron Kuris <ron.kuris@avalabs.org>
2023-08-22 15:59:08 +0000

    Make Nibbles newtype wrapped non-pub
    Have to construct it with a new method now

--- firewood/src/nibbles.rs
+++ firewood/src/nibbles.rs
@@ -32,7 +32,7 @@ static NIBBLES: [u8; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
 /// # }
 /// ```
 #[derive(Debug)]
-pub struct Nibbles<'a, const LEADING_ZEROES: usize>(pub &'a [u8]);
+pub struct Nibbles<'a, const LEADING_ZEROES: usize>(&'a [u8]);
 
 impl<'a, const LEADING_ZEROES: usize> Index<usize> for Nibbles<'a, LEADING_ZEROES> {
     type Output = u8;
@@ -62,6 +62,10 @@ impl<'a, const LEADING_ZEROES: usize> Nibbles<'a, LEADING_ZEROES> {
     #[must_use]
     pub fn is_empty(&self) -> bool {
         LEADING_ZEROES == 0 && self.0.is_empty()
+    }
+
+    pub fn new(inner: &'a [u8]) -> Self {
+        Nibbles(inner)
     }
 }
 
