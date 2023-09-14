1ef3409a52c3817378b8d8a0a8d304bbc29f8dd2
hhao <hao.hao@avalabs.org>
2023-09-14 16:59:02 +0000

    ensure the coding result is as expected

--- firewood/tests/codec.rs
+++ firewood/tests/codec.rs
@@ -4,14 +4,16 @@ use firewood::codec::{decode_int, decode_str, encode_int, encode_str};
 fn test_bincode() {
     let val: i64 = 11;
     let encoded = encode_int(val).unwrap();
-    println!("encoded: {:?}", encoded);
+    let expected = vec![22];
+    assert_eq!(encoded, expected);
 
     let decoded: i64 = decode_int(&encoded[..]).unwrap();
     assert_eq!(val, decoded);
 
     let val = "hello world";
     let encoded = encode_str(val.as_bytes()).unwrap();
-    println!("encoded: {:?}", encoded);
+    let expected = vec![22, 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100];
+    assert_eq!(encoded, expected);
 
     let decoded: String = decode_str(&encoded[..]).unwrap();
     assert_eq!(val, decoded);
