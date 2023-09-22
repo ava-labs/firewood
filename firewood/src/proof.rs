e66cba1b9c87f772f39bc82b6c79420a83c7ae49
Richard Pringle <richard.pringle@avalabs.org>
2023-09-22 14:39:25 -0400

    fixup! fixup! fixup! fixup! WIP

--- firewood/src/proof.rs
+++ firewood/src/proof.rs
@@ -151,7 +151,7 @@ impl<N: AsRef<[u8]> + Send> Proof<N> {
     ) -> Result<(Option<SubProof>, NibblesIterator<'a, 0>), ProofError> {
         // let rlp = rlp::Rlp::new(rlp_encoded_node);
         let items: Vec<Encoded<Vec<u8>>> =
-            dbg!(bincode::DefaultOptions::new().deserialize(rlp_encoded_node))
+            dbg!(bincode::DefaultOptions::new().deserialize(dbg!(rlp_encoded_node)))
                 .map_err(|e| ProofError::DecodeError(e))?;
 
         // match rlp.item_count() {
