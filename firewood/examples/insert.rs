928d803a713908ccbb88ab2622c31c8418c33700
Ron Kuris <ron.kuris@avalabs.org>
2023-10-10 18:43:33 +0000

    Add clap support for insert example
    You can now specify command line arguments:
    
    	Usage: insert [OPTIONS]
    
    	Options:
    	  -k, --keylen <KEYLEN>          [default: 1-64]
    	  -d, --datalen <DATALEN>        [default: 32]
    	  -b, --batch-keys <BATCH_KEYS>  [default: 1]
    	  -i, --inserts <INSERTS>        [default: 100]
    	  -h, --help                     Print help

--- firewood/examples/insert.rs
+++ firewood/examples/insert.rs
@@ -1,7 +1,8 @@
 // Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE.md for licensing terms.
 
-use std::{error::Error, ops::Range, sync::Arc, time::Instant};
+use clap::Parser;
+use std::{error::Error, ops::RangeInclusive, sync::Arc, time::Instant};
 
 use firewood::{
     db::{Batch, BatchOp, Db, DbConfig},
@@ -9,34 +10,56 @@ use firewood::{
 };
 use rand::{distributions::Alphanumeric, Rng};
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, default_value = "1-64", value_parser = string_to_range)]
+    keylen: RangeInclusive<usize>,
+    #[arg(short, long, default_value = "32", value_parser = string_to_range)]
+    datalen: RangeInclusive<usize>,
+    #[arg(short, long, default_value_t = 1)]
+    batch_keys: usize,
+    #[arg(short, long, default_value_t = 100)]
+    inserts: usize,
+}
+
+fn string_to_range(input: &str) -> Result<RangeInclusive<usize>, Box<dyn Error + Sync + Send>> {
+    //<usize as std::str::FromStr>::Err> {
+    let parts: Vec<&str> = input.split('-').collect();
+    match parts.len() {
+        1 => Ok(input.parse()?..=input.parse()?),
+        2 => Ok(parts[0].parse()?..=parts[1].parse()?),
+        _ => Err("Too many dashes in input string".into()),
+    }
+}
+
 /// cargo run --release --example insert
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
-    const KEYLEN: Range<usize> = 1..64;
-    const DATALEN: usize = 32;
-    const TOTAL_INSERTS: u32 = 100;
-    const KEYS_PER_INSERT: usize = 2;
-
     let cfg = DbConfig::builder().truncate(true).build();
+
+    let args = Args::parse();
 
     let db = tokio::task::spawn_blocking(move || {
         Db::new("rev_db", &cfg).expect("db initiation should succeed")
     })
     .await
     .unwrap();
+
+    let keys = args.batch_keys;
     let start = Instant::now();
-    for _ in 0..TOTAL_INSERTS {
-        let keylen = rand::thread_rng().gen_range(KEYLEN);
-        let batch: Batch<Vec<u8>, Vec<u8>> = (0..KEYS_PER_INSERT)
+    for _ in 0..args.inserts {
+        let keylen = rand::thread_rng().gen_range(args.keylen.clone());
+        let datalen = rand::thread_rng().gen_range(args.datalen.clone());
+        let batch: Batch<Vec<u8>, Vec<u8>> = (0..keys)
             .map(|_| {
                 (
                     rand::thread_rng()
                         .sample_iter(&Alphanumeric)
-                        .take(keylen)
+                        .take(keylen as usize)
                         .collect::<Vec<u8>>(),
                     rand::thread_rng()
                         .sample_iter(&Alphanumeric)
-                        .take(DATALEN)
+                        .take(datalen as usize)
                         .collect::<Vec<u8>>(),
                 )
             })
@@ -46,6 +69,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         proposal.commit().await?;
     }
     let duration = start.elapsed();
-    println!("Inserted {TOTAL_INSERTS} in {duration:?}");
+    println!("Inserted {keys} in {duration:?}");
     Ok(())
 }
