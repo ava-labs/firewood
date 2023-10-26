d6d25002f1fe9a2e44ff65e8189f24945886074d
Ron Kuris <ron.kuris@avalabs.org>
2023-10-26 23:25:42 +0000

    Add license header

--- firewood/tests/common/mod.rs
+++ firewood/tests/common/mod.rs
@@ -1,3 +1,6 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE.md for licensing terms.
+
 use std::{env::temp_dir, fs::remove_dir_all, ops::Deref, path::PathBuf};
 
 use firewood::db::{Db, DbConfig};
