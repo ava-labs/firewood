fdaa2af53a763fe771508369f340ef1b2893fe35
Ron Kuris <ron.kuris@avalabs.org>
2023-11-27 21:39:30 +0000

    Growth ring unwrap cleanups
    Pushed some down the stack and removed others

--- growth-ring/examples/demo1.rs
+++ growth-ring/examples/demo1.rs
@@ -9,26 +9,29 @@ use growthring::{
 };
 use rand::{seq::SliceRandom, Rng, SeedableRng};
 
-fn test(records: Vec<String>, wal: &mut WalWriter<WalFileImpl, WalStoreImpl>) -> Vec<WalRingId> {
+fn test(
+    records: Vec<String>,
+    wal: &mut WalWriter<WalFileImpl, WalStoreImpl>,
+) -> Result<Vec<WalRingId>, ()> {
     let mut res = Vec::new();
     for r in wal.grow(records).into_iter() {
-        let ring_id = futures::executor::block_on(r).unwrap().1;
+        let ring_id = futures::executor::block_on(r)?.1;
         println!("got ring id: {:?}", ring_id);
         res.push(ring_id);
     }
-    res
+    Ok(res)
 }
 
 fn recover(payload: WalBytes, ringid: WalRingId) -> Result<(), WalError> {
     println!(
         "recover(payload={}, ringid={:?}",
-        std::str::from_utf8(&payload).unwrap(),
+        std::str::from_utf8(&payload).map_err(|e| WalError::Other(e.to_string()))?,
         ringid
     );
     Ok(())
 }
 
-fn main() {
+fn main() -> Result<(), WalError> {
     let wal_dir = "./wal_demo1";
     let mut rng = rand::rngs::StdRng::seed_from_u64(0);
     let mut loader = WalLoader::new();
@@ -81,7 +84,10 @@ fn main() {
                 }
                 records.push(rec)
             }
-            for id in test(records, &mut wal).iter() {
+            for id in test(records, &mut wal)
+                .map_err(|_| WalError::Other("test failed".to_string()))?
+                .iter()
+            {
                 ids.push(*id)
             }
         }
@@ -100,4 +106,6 @@ fn main() {
         assert_eq!(std::str::from_utf8(&rec).unwrap(), &ans);
         println!("{}", std::str::from_utf8(&rec).unwrap());
     }
+
+    Ok(())
 }
