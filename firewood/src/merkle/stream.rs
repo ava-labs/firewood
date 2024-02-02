9f8281261b953a890295fe9a1577849a43aabab0
Dan Laine <daniel.laine@avalabs.org>
2024-02-02 10:49:20 -0500

    remove errantly added WIP code

--- firewood/src/merkle/stream.rs
+++ firewood/src/merkle/stream.rs
@@ -464,20 +464,6 @@ mod tests {
             }
         }
 
-        // let mut merkle = create_test_merkle();
-        // let root = merkle.init_root().unwrap();
-
-        // // Insert key-values in reverse order to ensure iterator
-        // // doesn't just return the keys in insertion order.
-        // for i in (0..=u8::MAX).rev() {
-        //     for j in (0..=u8::MAX).rev() {
-        //         let key = vec![i, j];
-        //         let value = vec![i, j];
-
-        //         merkle.insert(key, value, root).unwrap();
-        //     }
-        // }
-
         // Test with no start key
         let mut stream = merkle.iter(root);
         for i in 0..u8::MAX {
@@ -495,32 +481,6 @@ mod tests {
             }
         }
         check_stream_is_done(stream).await;
-
-        // Test with start key
-        for i in 0..=u8::MAX {
-            let mut stream = merkle.iter_from(root, vec![i].into_boxed_slice());
-            for j in 0..=u8::MAX {
-                let expected_key = vec![i, j];
-                let expected_value = vec![i, j];
-                assert_eq!(
-                    stream.next().await.unwrap().unwrap(),
-                    (expected_key.into_boxed_slice(), expected_value),
-                    "i: {}, j: {}",
-                    i,
-                    j,
-                );
-            }
-            if i == u8::MAX {
-                check_stream_is_done(stream).await;
-            } else {
-                assert_eq!(
-                    stream.next().await.unwrap().unwrap(),
-                    (vec![i + 1, 0].into_boxed_slice(), vec![i + 1, 0]),
-                    "i: {}",
-                    i,
-                );
-            }
-        }
     }
 
     #[tokio::test]
