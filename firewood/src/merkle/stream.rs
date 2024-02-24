4255e5b36d9876b5a6ac8ad16614a59ba48e655c
Ron Kuris <ron.kuris@avalabs.org>
2024-02-24 00:09:44 +0000

    Format

--- firewood/src/merkle/stream.rs
+++ firewood/src/merkle/stream.rs
@@ -32,8 +32,15 @@ enum IterationNode<'a> {
 impl<'a> std::fmt::Debug for IterationNode<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Unvisited { key, node } => f.debug_struct("Unvisited").field("key", key).field("node", node).finish(),
-            Self::Visited { key, children_iter : _} => f.debug_struct("Visited").field("key", key).finish(),
+            Self::Unvisited { key, node } => f
+                .debug_struct("Unvisited")
+                .field("key", key)
+                .field("node", node)
+                .finish(),
+            Self::Visited {
+                key,
+                children_iter: _,
+            } => f.debug_struct("Visited").field("key", key).finish(),
         }
     }
 }
