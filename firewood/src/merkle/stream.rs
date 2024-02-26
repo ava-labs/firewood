a6229819908b87c9497a73aa27dbe783d9c3cf79
Ron Kuris <ron.kuris@avalabs.org>
2024-02-26 22:09:44 +0000

    Revert visibility change

--- firewood/src/merkle/stream.rs
+++ firewood/src/merkle/stream.rs
@@ -328,7 +328,7 @@ impl<'a, S: ShaleStore<Node> + Send + Sync, T> FusedStream for MerkleKeyValueStr
 }
 
 impl<'a, S, T> MerkleKeyValueStream<'a, S, T> {
-    pub(crate) fn new(merkle: &'a Merkle<S, T>, merkle_root: DiskAddress) -> Self {
+    pub(super) fn new(merkle: &'a Merkle<S, T>, merkle_root: DiskAddress) -> Self {
         Self {
             state: MerkleKeyValueStreamState::new(),
             merkle_root,
