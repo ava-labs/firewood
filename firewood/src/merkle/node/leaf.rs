524b7733cd57028f452d5bd7138fc884ff4d77c2
Dan Laine <daniel.laine@avalabs.org>
2024-03-01 12:29:31 -0500

    fix test

--- firewood/src/merkle/node/leaf.rs
+++ firewood/src/merkle/node/leaf.rs
@@ -149,9 +149,12 @@ mod tests {
     use test_case::test_case;
 
     // these tests will fail if the encoding mechanism changes and should be updated accordingly
-    #[test_case(0b10 << 4, vec![0x12, 0x34], vec![1, 2, 3, 4]; "even length")]
-    // first nibble is part of the prefix
-    #[test_case((0b11 << 4) + 2, vec![0x34], vec![2, 3, 4]; "odd length")]
+    //
+    // Even length so ODD_LEN flag is not set so flag byte is 0b0000_0000
+    #[test_case(0x00, vec![0x12, 0x34], vec![1, 2, 3, 4]; "even length")]
+    // Odd length so ODD_LEN flag is set so flag byte is 0b0000_0001
+    // This is combined with the first nibble of the path (0b0000_0010) to become 0b0001_0010
+    #[test_case(0b0001_0010, vec![0x34], vec![2, 3, 4]; "odd length")]
     fn encode_regression_test(prefix: u8, path: Vec<u8>, nibbles: Vec<u8>) {
         let data = vec![5, 6, 7, 8];
 
