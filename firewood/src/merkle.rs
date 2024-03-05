8acecd49641edf3af79e19f2680d1e9800878aec
Dan Laine <daniel.laine@avalabs.org>
2024-03-05 12:07:44 -0500

    Revert "cleanup"
    This reverts commit d97188f0490ed42cd85b9b51433ee05b11e40e37.

--- firewood/src/merkle.rs
+++ firewood/src/merkle.rs
@@ -119,26 +119,26 @@ where
                 let encoded_children = n.chd().iter().zip(n.children_encoded.iter());
                 let children = encoded_children
                     .map(|(child_addr, encoded_child)| {
-                        encoded_child
-                            .as_ref()
-                            .map(|node_digest| Ok(node_digest.to_vec()))
-                            .or_else(|| {
-                                // Calculate the encoded bytes (and possibly hash) for this node.
-                                child_addr
-                                    // if there's a child disk address here, get the encoded bytes
-                                    .map(|addr| {
-                                        self.get_node(addr)
-                                            .and_then(|node| self.encode(node.inner()))
-                                            .map(|node_bytes| {
-                                                if node_bytes.len() >= TRIE_HASH_LEN {
-                                                    Keccak256::digest(&node_bytes).to_vec()
-                                                } else {
-                                                    node_bytes
-                                                }
-                                            })
-                                    })
-                            })
-                            .transpose()
+                        if encoded_child.is_some() {
+                            // We already have the encoded bytes or hash for this node.
+                            Ok(encoded_child.clone())
+                        } else {
+                            // Calculate the encoded bytes (and possibly hash) for this node.
+                            child_addr
+                                // if there's a child disk address here, get the encoded bytes
+                                .map(|addr| {
+                                    self.get_node(addr)
+                                        .and_then(|node| self.encode(node.inner()))
+                                        .map(|node_bytes| {
+                                            if node_bytes.len() >= TRIE_HASH_LEN {
+                                                Keccak256::digest(&node_bytes).to_vec()
+                                            } else {
+                                                node_bytes
+                                            }
+                                        })
+                                })
+                                .transpose()
+                        }
                     })
                     .collect::<Result<Vec<Option<Vec<u8>>>, MerkleError>>()?
                     .try_into()
