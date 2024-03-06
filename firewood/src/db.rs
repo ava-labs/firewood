fc2281a4a1f09ef0cc1c947e6ace6a7b886d834f
Dan Laine <daniel.laine@avalabs.org>
2024-03-06 16:22:29 -0500

    clean up new_internal

--- firewood/src/db.rs
+++ firewood/src/db.rs
@@ -510,47 +510,32 @@ impl Db {
         let merkle_path = file::touch_dir("merkle", &db_path)?;
         let merkle_meta_path = file::touch_dir("meta", &merkle_path)?;
         let merkle_payload_path = file::touch_dir("compact", &merkle_path)?;
-
         let root_hash_path = file::touch_dir("root_hash", &db_path)?;
 
-        let file0 = crate::file::File::new(0, SPACE_RESERVED, &merkle_meta_path)?;
-        let fd0 = file0.as_fd();
+        let meta_file = crate::file::File::new(0, SPACE_RESERVED, &merkle_meta_path)?;
+        let meta_fd = meta_file.as_fd();
 
         if reset {
-            // initialize dbparams
+            // initialize DbParams
             if cfg.payload_file_nbit < cfg.payload_regn_nbit
                 || cfg.payload_regn_nbit < PAGE_SIZE_NBIT
             {
                 return Err(DbError::InvalidParams);
             }
-            Self::initialize_header_on_disk(&cfg, fd0)?;
+            Self::initialize_header_on_disk(&cfg, meta_fd)?;
         }
 
         // read DbParams
         let mut header_bytes = [0; size_of::<DbParams>()];
-        nix::sys::uio::pread(fd0, &mut header_bytes, 0).map_err(DbError::System)?;
-        drop(file0);
+        nix::sys::uio::pread(meta_fd, &mut header_bytes, 0).map_err(DbError::System)?;
+        drop(meta_file);
         #[allow(clippy::indexing_slicing)]
         let params: DbParams = cast_slice(&header_bytes)[0];
 
-        let wal = WalConfig::builder()
-            .file_nbit(params.wal_file_nbit)
-            .block_nbit(params.wal_block_nbit)
-            .max_revisions(cfg.wal.max_revisions)
-            .build();
         let (sender, inbound) = tokio::sync::mpsc::unbounded_channel();
         let disk_requester = DiskBufferRequester::new(sender);
-        let buffer = cfg.buffer.clone();
-        #[allow(clippy::unwrap_used)]
-        let disk_buffer = DiskBuffer::new(inbound, &buffer, &wal).expect("DiskBuffer::new");
 
-        let disk_thread = Some(
-            std::thread::Builder::new()
-                .name("DiskBuffer".to_string())
-                .spawn(move || disk_buffer.run())
-                .expect("thread spawn should succeed"),
-        );
-
+        // set up caches
         #[allow(clippy::unwrap_used)]
         let root_hash_cache: Arc<CachedSpace> = CachedSpace::new(
             &StoreConfig::builder()
@@ -565,7 +550,6 @@ impl Db {
         .unwrap()
         .into();
 
-        // setup disk buffer
         #[allow(clippy::unwrap_used)]
         let data_cache = Universe {
             merkle: SubUniverse::<Arc<CachedSpace>>::new(
@@ -609,9 +593,6 @@ impl Db {
         // recover from Wal
         disk_requester.init_wal("wal", &db_path);
 
-        let root_hash_staging = StoreRevMut::new(root_hash_cache);
-        let reset_headers = reset;
-
         let base = Universe {
             merkle: get_sub_universe_from_empty_delta(&data_cache.merkle),
         };
@@ -631,13 +612,28 @@ impl Db {
             &cfg.rev,
         )?;
 
+        let wal_config = WalConfig::builder()
+            .file_nbit(params.wal_file_nbit)
+            .block_nbit(params.wal_block_nbit)
+            .max_revisions(cfg.wal.max_revisions)
+            .build();
+
+        #[allow(clippy::unwrap_used)]
+        let disk_buffer =
+            DiskBuffer::new(inbound, &cfg.buffer, &wal_config).expect("DiskBuffer::new");
+
         Ok(Self {
             inner: Arc::new(RwLock::new(DbInner {
-                disk_thread,
+                disk_thread: Some(
+                    std::thread::Builder::new()
+                        .name("DiskBuffer".to_string())
+                        .spawn(move || disk_buffer.run())
+                        .expect("thread spawn should succeed"),
+                ),
                 disk_requester,
                 cached_space: data_cache,
-                reset_store_headers: reset_headers,
-                root_hash_staging,
+                reset_store_headers: reset,
+                root_hash_staging: StoreRevMut::new(root_hash_cache),
             })),
             revisions: Arc::new(Mutex::new(DbRevInner {
                 inner: VecDeque::new(),
