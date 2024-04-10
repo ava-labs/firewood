7ec481dd735d842f3592d40c6946f94c2c4ec537
Dan Laine <daniel.laine@avalabs.org>
2024-04-10 18:20:00 +0000

    refactor test_in_mem_write_linear_store

--- firewood/src/storage/linear/mod.rs
+++ firewood/src/storage/linear/mod.rs
@@ -150,32 +150,26 @@ pub mod tests {
         }
     }
 
-    #[test_case(&[],0, &[1, 2, 3],&[1, 2, 3]; "write to empty store")]
-    #[test_case(&[1, 2, 3],3, &[4, 5, 6],&[1, 2, 3, 4, 5, 6]; "write at end of store")]
-    #[test_case(&[1, 2, 3],0, &[7, 8],&[7, 8, 3]; "overwrite beginning of store")]
-    #[test_case(&[1, 2, 3],1, &[9],&[1, 9, 3]; "overwrite middle of store")]
-    #[test_case(&[1, 2, 3],3, &[11, 12],&[1, 2, 3,  11, 12]; "write past end of store")]
-    fn test_in_mem_write_linear_store(
-        start_bytes: &[u8],
-        offset: u64,
-        new_bytes: &[u8],
-        expected_end_bytes: &[u8],
-    ) {
+    #[test_case(&[(0,&[1, 2, 3])],(0,&[1, 2, 3]); "write to empty store")]
+    #[test_case(&[(0,&[1, 2, 3])],(1,&[2, 3]); "read from middle of store")]
+    #[test_case(&[(0,&[1, 2, 3])],(2,&[3]); "read from end of store")]
+    #[test_case(&[(0,&[1, 2, 3])],(3,&[]); "read past end of store")]
+    #[test_case(&[(0,&[1, 2, 3]),(3,&[4,5,6])],(0,&[1, 2, 3,4,5,6]); "write to end of store")]
+    #[test_case(&[(0,&[1, 2, 3]),(0,&[4])],(0,&[4,2,3]); "overwrite start of store")]
+    #[test_case(&[(0,&[1, 2, 3]),(1,&[4])],(0,&[1,4,3]); "overwrite middle of store")]
+    #[test_case(&[(0,&[1, 2, 3]),(2,&[4])],(0,&[1,2,4]); "overwrite end of store")]
+    #[test_case(&[(0,&[1, 2, 3]),(2,&[4,5])],(0,&[1,2,4,5]); "overwrite/extend end of store")]
+    fn test_in_mem_write_linear_store(writes: &[(u64, &[u8])], expected: (u64, &[u8])) {
         let mut store = InMemReadWriteLinearStore { bytes: vec![] };
         assert_eq!(store.size().unwrap(), 0);
 
-        store.write(0, start_bytes).unwrap();
-        assert_eq!(store.bytes, start_bytes);
-
-        store.write(offset, new_bytes).unwrap();
-        assert_eq!(store.bytes, expected_end_bytes);
-
-        for i in 0..store.size().unwrap() {
-            let mut reader = store.stream_from(i).unwrap();
-            let mut read_bytes = vec![];
-            let num_read_bytes = reader.read_to_end(&mut read_bytes).unwrap();
-            assert_eq!(read_bytes, expected_end_bytes[i as usize..]);
-            assert_eq!(num_read_bytes, expected_end_bytes.len() - i as usize);
+        for write in writes {
+            store.write(write.0, write.1).unwrap();
         }
+
+        let mut reader = store.stream_from(expected.0).unwrap();
+        let mut read_bytes = vec![];
+        reader.read_to_end(&mut read_bytes).unwrap();
+        assert_eq!(read_bytes, expected.1);
     }
 }
