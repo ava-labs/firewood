4553eeb782c275c94608df0a38473ce66213bf88
Dan Laine <daniel.laine@avalabs.org>
2024-04-23 20:09:57 +0000

    delete shale folder

--- firewood/src/lib.rs
+++ firewood/src/lib.rs
@@ -189,8 +189,6 @@ pub mod storage;
 
 pub mod config;
 pub mod nibbles;
-// TODO: shale should not be pub, but there are integration test dependencies :(
-pub mod shale;
 
 pub mod logger;
 pub mod v2;
--- firewood/src/shale/mod.rs
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE.md for licensing terms.
-
-// pub(crate) use disk_address::DiskAddress;
-
-// pub mod disk_address;
