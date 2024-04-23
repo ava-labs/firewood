aa76050aea3e1f1da494cbcc17a7c7ef52f8536a
hhao <hao.hao@avalabs.org>
2024-04-23 06:59:10 +0000

    Remove immature page cache and clean up

--- firewood/src/storage/persisted/mod.rs
+++ firewood/src/storage/persisted/mod.rs
@@ -5,16 +5,11 @@
 #![allow(dead_code)]
 
 use bytes::Bytes;
-use std::collections::HashMap;
 use std::io::{Error, SeekFrom};
 use std::path::PathBuf;
 use tokio::fs::{File, OpenOptions};
 use tokio::io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt};
 use tokio::sync::Mutex;
-
-// Page should be boxed as to not take up so much stack-space
-type Page = Box<[u8; PAGE_SIZE as usize]>;
-const PAGE_SIZE: usize = 4096; // Page size in bytes
 
 #[derive(Debug)]
 pub struct StoreWrite {
@@ -25,7 +20,6 @@ pub struct StoreWrite {
 pub struct PersistedStore {
     path: PathBuf,
     fd: Mutex<File>,
-    cache: Mutex<HashMap<u64, Page>>, // Page cache storing page offset and data
 }
 
 impl PersistedStore {
@@ -41,53 +35,16 @@ impl PersistedStore {
         Ok(Self {
             path,
             fd: Mutex::new(file),
-            cache: Mutex::new(HashMap::new()),
         })
     }
 
     // TODO: impl ReadLinearStore when it is async
     pub async fn stream_from(&self, addr: u64, length: usize) -> Result<Bytes, Error> {
-        let start_page = addr / PAGE_SIZE as u64;
-        let end_page = (addr + length as u64 - 1) / PAGE_SIZE as u64;
-
-        let mut buffer = Vec::with_capacity(length);
-
-        for page in start_page..=end_page {
-            let page_offset = page * PAGE_SIZE as u64;
-            let page_data = self.get_page(page_offset).await?;
-            let offset_within_page = if page == start_page {
-                (addr % PAGE_SIZE as u64) as usize
-            } else {
-                0
-            };
-            let remaining_length = (addr + length as u64 - page_offset) as usize;
-            let data_to_copy = std::cmp::min(remaining_length, PAGE_SIZE - offset_within_page);
-            buffer.extend_from_slice(
-                &page_data[offset_within_page..offset_within_page + data_to_copy],
-            );
-        }
-
-        Ok(Bytes::from(buffer))
-    }
-
-    async fn get_page(&self, page_offset: u64) -> Result<Page, Error> {
-        let mut cache = self.cache.lock().await;
-        if let Some(data) = cache.get(&page_offset) {
-            return Ok(data.clone());
-        }
-
         let mut file = self.fd.lock().await;
-        file.seek(SeekFrom::Start(page_offset)).await?; // Seek to the page offset
-
-        let file_size = file.metadata().await?.len(); // Get the size of the file
-        let remaining_bytes = file_size.saturating_sub(page_offset); // Calculate remaining bytes in the file
-        let page_size = std::cmp::min(PAGE_SIZE as u64, remaining_bytes) as usize; // Choose minimum of PAGE_SIZE and remaining bytes
-        let mut page_data = Box::new([0u8; PAGE_SIZE]); // Initialize page_data with zeros
-
-        file.read_exact(&mut page_data[..page_size]).await?; // Read data into page_data
-
-        cache.insert(page_offset, page_data.clone());
-        Ok(page_data)
+        file.seek(SeekFrom::Start(addr)).await?;
+        let mut buffer = vec![0; length];
+        file.read_exact(&mut buffer).await?;
+        Ok(Bytes::from(buffer))
     }
 
     pub async fn write_all(&mut self, writes: &[StoreWrite]) -> Result<(), Error> {
@@ -114,25 +71,30 @@ mod tests {
         let file_path = temp_dir.join("test_persisted_file");
 
         let mut persisted_store = PersistedStore::new(file_path.clone()).await.unwrap();
+        let first_data = "Hello, ";
+        let second_data = "world";
 
         let writes = vec![
             StoreWrite {
                 offset: 0,
-                data: Bytes::from_static(b"Hello, "),
+                data: Bytes::from(first_data),
             },
             StoreWrite {
-                offset: 7,
-                data: Bytes::from_static(b"world!"),
+                offset: first_data.len() as u64,
+                data: Bytes::from(second_data),
             },
         ];
 
         persisted_store.write_all(&writes).await.unwrap();
 
-        let read_data = persisted_store.stream_from(0, 13).await.unwrap();
+        let read_data = persisted_store
+            .stream_from(0, first_data.len() + second_data.len())
+            .await
+            .unwrap();
         let read_str = String::from_utf8(read_data.to_vec()).unwrap();
 
         // Verify that the read data matches the expected content
-        assert_eq!(read_str, "Hello, world!");
+        assert_eq!(read_str, first_data.to_owned() + second_data);
 
         // Clean up by deleting the temporary file
         fs::remove_file(file_path)
