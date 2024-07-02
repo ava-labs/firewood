23d9cd7795701e61c3cd4ed5e8d6c6d538c9841e
Dan Laine <daniel.laine@avalabs.org>
2024-07-02 10:22:11 -0400

    consolidate children_iter impl for BranchNode

--- firewood/src/merkle.rs
+++ firewood/src/merkle.rs
@@ -130,14 +130,10 @@ impl<T: ReadLinearStore> Merkle<T> {
 
             let mut child_hashes: [Option<TrieHash>; BranchNode::MAX_CHILDREN] = Default::default();
             if let Some(branch) = root.as_branch() {
-                for (i, child) in branch.children.iter().enumerate() {
-                    // TODO danlaine: can we avoid indexing?
-                    #[allow(clippy::indexing_slicing)]
-                    match child {
-                        Child::None => {}
-                        Child::Address(_) => unreachable!("TODO is this reachable?"),
-                        Child::AddressWithHash(_, hash) => child_hashes[i] = Some(hash.clone()),
-                    }
+                // TODO danlaine: can we avoid indexing?
+                #[allow(clippy::indexing_slicing)]
+                for (i, hash) in branch.children_iter() {
+                    child_hashes[i] = Some(hash.clone());
                 }
             }
 
