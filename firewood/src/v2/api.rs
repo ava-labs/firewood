63581f1734694510a2d0d25991486c723524ccdf
Dan Laine <daniel.laine@avalabs.org>
2024-08-13 14:31:22 -0400

    remove unused error

--- firewood/src/v2/api.rs
+++ firewood/src/v2/api.rs
@@ -75,9 +75,6 @@ pub enum Error {
         end_key: Box<[u8]>,
     },
 
-    #[error("limit must be positive")]
-    InvalidLimit,
-
     #[error("IO error: {0}")]
     IO(#[from] std::io::Error),
 
