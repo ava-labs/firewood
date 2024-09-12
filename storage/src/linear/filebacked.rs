f31b363053d7fb55cbba4d7dce3fbc4a1dc42686
Ron Kuris <ron.kuris@avalabs.org>
2024-09-12 08:01:11 -1000

    Revert "Do not cache leaf nodes"
    This reverts commit c5bec88f6743822336316b162c48e533214f9f47.

--- storage/src/linear/filebacked.rs
+++ storage/src/linear/filebacked.rs
@@ -98,9 +98,7 @@ impl WritableStorage for FileBacked {
     ) -> Result<(), Error> {
         let mut guard = self.cache.lock().expect("poisoned lock");
         for (addr, node) in nodes {
-            if matches!(**node, Node::Branch(_)) {
-                guard.put(*addr, node.clone());
-            }
+            guard.put(*addr, node.clone());
         }
         Ok(())
     }
