7d0b90471182f916a49f4deeeb9e2b6f4b131c2b
qusuyan <qusuyan@gmail.com>
2025-06-06 10:55:40 -0400

    feat(fsck): range set return complement

--- storage/src/range_set.rs
+++ storage/src/range_set.rs
@@ -1,9 +1,8 @@
 // Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE.md for licensing terms.
 
-use btree_range_map::{AnyRange, AsRange, RangeSet};
-use std::num::NonZeroU64;
-use std::ops::Bound;
+use btree_range_map::{AnyRange, RangeSet};
+use std::ops::{Bound, Range};
 
 use crate::nodestore::NodeStore;
 use crate::{CheckerError, LinearAddress};
@@ -32,7 +31,7 @@ impl LinearAddressRangeSet {
     ) -> Result<(), CheckerError> {
         let start = addr.get();
         // end can be larger than the file size: only the node in the stored area is written to disk
-        if start < NodeStore::HEADER_SIZE || start > self.size {
+        if start < NodeStore::HEADER_SIZE || start + size > self.size {
             return Err(CheckerError::AreaOutOfBounds { start: addr, size });
         }
         if self.range_set.intersects(start..start + size) {
@@ -41,26 +40,49 @@ impl LinearAddressRangeSet {
         self.range_set.insert(start..start + size);
         Ok(())
     }
+
+    #[expect(clippy::unwrap_used)]
+    pub(super) fn complement(self) -> Vec<Range<LinearAddress>> {
+        let mut complement = Vec::new();
+
+        let mut cur_start = LinearAddress::new(NodeStore::HEADER_SIZE).unwrap();
+        let db_end = LinearAddress::new(self.size).unwrap();
+
+        for Range { start, end } in self.into_iter() {
+            // skip empty ranges - this may happen for the first range
+            debug_assert!(cur_start <= start);
+            if cur_start < start {
+                complement.push(cur_start..start);
+            }
+            cur_start = end;
+        }
+
+        if cur_start != db_end {
+            complement.push(cur_start..db_end);
+        }
+
+        complement
+    }
 }
 
 impl IntoIterator for LinearAddressRangeSet {
-    type Item = (LinearAddress, u64);
-    type IntoIter = std::iter::Map<
-        <RangeSet<u64> as IntoIterator>::IntoIter,
-        fn(AnyRange<u64>) -> (LinearAddress, u64),
-    >;
+    type Item = Range<LinearAddress>;
+    type IntoIter =
+        std::iter::Map<<RangeSet<u64> as IntoIterator>::IntoIter, fn(AnyRange<u64>) -> Self::Item>;
 
+    #[expect(clippy::unwrap_used)]
     fn into_iter(self) -> Self::IntoIter {
-        self.range_set.into_iter().map(|range| {
-            let Bound::Included(start) = range.start() else {
+        self.range_set.into_iter().map(|AnyRange { start, end }| {
+            let Bound::Included(start) = start else {
                 panic!("start of range is not included, this should not happen");
             };
-            let Bound::Excluded(end) = range.end() else {
+            let Bound::Excluded(end) = end else {
                 panic!("end of range is not excluded, this should not happen");
             };
-            let start_addr =
-                NonZeroU64::new(*start).expect("found zero address, this should not happen");
-            (start_addr, end - start)
+            Range {
+                start: LinearAddress::new(start).unwrap(),
+                end: LinearAddress::new(end).unwrap(),
+            }
         })
     }
 }
@@ -74,25 +96,35 @@ mod tests {
     #[test]
     fn test_empty() {
         let visited = LinearAddressRangeSet::new(0x1000).unwrap();
-        assert_eq!(visited.into_iter().collect::<Vec<_>>(), vec![]);
+        let visited_ranges = visited
+            .into_iter()
+            .map(|range| (range.start, range.end))
+            .collect::<Vec<_>>();
+        assert_eq!(visited_ranges, vec![]);
     }
 
     #[test]
     fn test_insert_area() {
-        let mut visited = LinearAddressRangeSet::new(0x1000).unwrap();
-        let start = LinearAddress::new(2048).unwrap();
+        let start = 2048;
         let size = 1024;
 
+        let start_addr = LinearAddress::new(start).unwrap();
+        let end_addr = LinearAddress::new(start + size).unwrap();
+
+        let mut visited = LinearAddressRangeSet::new(0x1000).unwrap();
         visited
-            .insert_area(start, size)
+            .insert_area(start_addr, size)
             .expect("the given area should be within bounds");
 
-        assert_eq!(visited.into_iter().collect::<Vec<_>>(), vec![(start, size)]);
+        let visited_ranges = visited
+            .into_iter()
+            .map(|range| (range.start, range.end))
+            .collect::<Vec<_>>();
+        assert_eq!(visited_ranges, vec![(start_addr, end_addr)]);
     }
 
     #[test]
     fn test_consecutive_areas_merge() {
-        let mut visited = LinearAddressRangeSet::new(0x1000).unwrap();
         let start1 = 2048;
         let size1 = 1024;
         let start2 = start1 + size1;
@@ -100,7 +132,9 @@ mod tests {
 
         let start1_addr = LinearAddress::new(start1).unwrap();
         let start2_addr = LinearAddress::new(start2).unwrap();
+        let end2_addr = LinearAddress::new(start2 + size2).unwrap();
 
+        let mut visited = LinearAddressRangeSet::new(0x1000).unwrap();
         visited
             .insert_area(start1_addr, size1)
             .expect("the given area should be within bounds");
@@ -109,10 +143,11 @@ mod tests {
             .insert_area(start2_addr, size2)
             .expect("the given area should be within bounds");
 
-        assert_eq!(
-            visited.into_iter().collect::<Vec<_>>(),
-            vec![(start1_addr, size1 + size2)]
-        );
+        let visited_ranges = visited
+            .into_iter()
+            .map(|range| (range.start, range.end))
+            .collect::<Vec<_>>();
+        assert_eq!(visited_ranges, vec![(start1_addr, end2_addr),]);
     }
 
     #[test]
@@ -165,5 +200,49 @@ mod tests {
 
         assert_eq!(err_start1_addr, start1_addr);
         assert_eq!(err_size1, size1);
+    }
+
+    #[test]
+    fn test_complement() {
+        let start1 = 3000;
+        let size1 = 1024;
+        let start2 = 4096;
+        let size2 = 1024;
+        let db_size = 0x2000;
+
+        let db_begin = LinearAddress::new(NodeStore::HEADER_SIZE).unwrap();
+        let start1_addr = LinearAddress::new(start1).unwrap();
+        let end1_addr = LinearAddress::new(start1 + size1).unwrap();
+        let start2_addr = LinearAddress::new(start2).unwrap();
+        let end2_addr = LinearAddress::new(start2 + size2).unwrap();
+        let db_end = LinearAddress::new(db_size).unwrap();
+
+        let mut visited = LinearAddressRangeSet::new(db_size).unwrap();
+        visited.insert_area(start1_addr, size1).unwrap();
+        visited.insert_area(start2_addr, size2).unwrap();
+
+        let complement = visited.complement();
+        assert_eq!(
+            complement,
+            vec![
+                db_begin..start1_addr,
+                end1_addr..start2_addr,
+                end2_addr..db_end,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_complement_with_full_range() {
+        let db_size = 0x1000;
+        let start = 2048;
+        let size = db_size - start;
+
+        let mut visited = LinearAddressRangeSet::new(db_size).unwrap();
+        visited
+            .insert_area(LinearAddress::new(start).unwrap(), size)
+            .unwrap();
+        let complement = visited.complement();
+        assert_eq!(complement, vec![]);
     }
 }
