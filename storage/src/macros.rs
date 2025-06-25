494b4b236ad848742ca4ff3c41b1c3a629820b61
Pavlo Kushneryk <levelup.kpi@gmail.com>
2025-06-25 14:46:36 +0200

    fix: fix review

--- storage/src/macros.rs
+++ storage/src/macros.rs
@@ -1,29 +1,34 @@
-
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE.md for licensing terms.
 /// Macro to register and use a metric with description and labels.
 /// This macro is a wrapper around the `metrics` crate's `counter!` and `describe_counter!`
 /// macros. It ensures that the description is registered only once.
 ///
 /// Usage:
-///   firewood_metric!("metric_name", "description", value_to_increment_by)
-///   firewood_metric!("metric_name", "description", value_to_increment_by, "label" => "value")
+///   firewood_metric!("metric_name", "description")
+///   firewood_metric!("metric_name", "description", "label" => "value")
+///
+/// Call `.increment(val)` or `.absolute(val)` on the result as appropriate.
 #[macro_export]
 macro_rules! firewood_metric {
     // With labels
-    ($name:expr, $desc:expr, $inc:expr, $($labels:tt)+) => {
+    ($name:expr, $desc:expr, $($labels:tt)+) => {
         {
-            static METRIC: std::sync::LazyLock<()> = std::sync::LazyLock::new(|| {
+            static ONCE: std::sync::Once = std::sync::Once::new();
+            ONCE.call_once(|| {
                 metrics::describe_counter!($name, $desc);
             });
-            metrics::counter!($name, $($labels)+).increment($inc);
+            metrics::counter!($name, $($labels)+)
         }
     };
     // No labels
-    ($name:expr, $desc:expr, $inc:expr) => {
+    ($name:expr, $desc:expr) => {
         {
-            static METRIC: std::sync::LazyLock<()> = std::sync::LazyLock::new(|| {
+            static ONCE: std::sync::Once = std::sync::Once::new();
+            ONCE.call_once(|| {
                 metrics::describe_counter!($name, $desc);
             });
-            metrics::counter!($name).increment($inc);
+            metrics::counter!($name)
         }
     };
 }
