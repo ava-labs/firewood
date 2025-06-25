c187580dffe0aa2286d8d459c8f6b6d8c9b84e56
Pavlo Kushneryk <levelup.kpi@gmail.com>
2025-06-25 20:58:24 +0200

    fix: fix review

--- storage/src/macros.rs
+++ storage/src/macros.rs
@@ -1,16 +1,16 @@
 // Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE.md for licensing terms.
+#[macro_export]
 /// Macro to register and use a metric with description and labels.
 /// This macro is a wrapper around the `metrics` crate's `counter!` and `describe_counter!`
 /// macros. It ensures that the description is registered only once.
 ///
 /// Usage:
-///   firewood_metric!("metric_name", "description")
-///   firewood_metric!("metric_name", "description", "label" => "value")
+///   firewood_counter!("metric_name", "description")
+///   firewood_counter!("metric_name", "description", "label" => "value")
 ///
 /// Call `.increment(val)` or `.absolute(val)` on the result as appropriate.
-#[macro_export]
-macro_rules! firewood_metric {
+macro_rules! firewood_counter {
     // With labels
     ($name:expr, $desc:expr, $($labels:tt)+) => {
         {
