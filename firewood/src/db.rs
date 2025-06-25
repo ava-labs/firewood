313cfc01835eea0f62fa19d3bc11f43b159be776
Ron Kuris <ron.kuris@avalabs.org>
2025-06-25 16:49:32 -0400

    feat: Add methods to fetch views from any hash
    There is a new method, view_sync, which gets a view from a specific
    hash.
    
    We need these in ffi to avoid locking issues in triedb due to
    asynchronous commit.
    
    Also changes the ffi so that get_from_root will also fetch from a
    proposal

--- firewood/src/db.rs
+++ firewood/src/db.rs
@@ -65,10 +65,39 @@ pub trait DbViewSync {
     fn val_sync<K: KeyType>(&self, key: K) -> Result<Option<Box<[u8]>>, DbError>;
 }
 
-impl DbViewSync for HistoricalRev {
+/// A synchronous view of the database with raw byte keys (object-safe version).
+pub trait DbViewSyncBytes {
+    /// find a value synchronously using raw bytes
+    fn val_sync_bytes(&self, key: &[u8]) -> Result<Option<Box<[u8]>>, DbError>;
+}
+
+// Provide blanket implementation for DbViewSync using DbViewSyncBytes
+impl<T: DbViewSyncBytes> DbViewSync for T {
     fn val_sync<K: KeyType>(&self, key: K) -> Result<Option<Box<[u8]>>, DbError> {
+        self.val_sync_bytes(key.as_ref())
+    }
+}
+
+impl DbViewSyncBytes for Arc<HistoricalRev> {
+    fn val_sync_bytes(&self, key: &[u8]) -> Result<Option<Box<[u8]>>, DbError> {
         let merkle = Merkle::from(self);
-        let value = merkle.get_value(key.as_ref())?;
+        let value = merkle.get_value(key)?;
+        Ok(value)
+    }
+}
+
+impl DbViewSyncBytes for Proposal<'_> {
+    fn val_sync_bytes(&self, key: &[u8]) -> Result<Option<Box<[u8]>>, DbError> {
+        let merkle = Merkle::from(self.nodestore.clone());
+        let value = merkle.get_value(key)?;
+        Ok(value)
+    }
+}
+
+impl DbViewSyncBytes for Arc<NodeStore<Arc<ImmutableProposal>, FileBacked>> {
+    fn val_sync_bytes(&self, key: &[u8]) -> Result<Option<Box<[u8]>>, DbError> {
+        let merkle = Merkle::from(self.clone());
+        let value = merkle.get_value(key)?;
         Ok(value)
     }
 }
@@ -273,6 +302,16 @@ impl Db {
         Ok(nodestore)
     }
 
+    /// Synchronously get a view, either committed or proposed
+    pub fn view_sync(&self, root_hash: TrieHash) -> Result<Box<dyn DbViewSyncBytes>, api::Error> {
+        let nodestore = self
+            .manager
+            .read()
+            .expect("poisoned lock")
+            .view(root_hash)?;
+        Ok(nodestore)
+    }
+
     /// propose a new batch synchronously
     pub fn propose_sync<K: KeyType, V: ValueType>(
         &'_ self,
@@ -432,12 +471,6 @@ impl Proposal<'_> {
         }
     }
 
-    /// Get a value from the proposal synchronously
-    pub fn val_sync<K: KeyType>(&self, key: K) -> Result<Option<Box<[u8]>>, api::Error> {
-        let merkle = Merkle::from(self.nodestore.clone());
-        merkle.get_value(key.as_ref()).map_err(api::Error::from)
-    }
-
     /// Create a new proposal from the current one synchronously
     pub fn propose_sync<K: KeyType, V: ValueType>(
         &self,
@@ -493,7 +526,7 @@ mod test {
     use std::ops::{Deref, DerefMut};
     use std::path::PathBuf;
 
-    use crate::db::Db;
+    use crate::db::{Db, DbViewSyncBytes as _};
     use crate::v2::api::{Db as _, DbView as _, Error, Proposal as _};
 
     use super::{BatchOp, DbConfig};
@@ -674,6 +707,47 @@ mod test {
         // through proposal3
         assert_eq!(&*proposal3.val(b"k2").await.unwrap().unwrap(), b"v2");
         assert_eq!(&*proposal3.val(b"k3").await.unwrap().unwrap(), b"v3");
+    }
+
+    #[tokio::test]
+    async fn test_view_sync() {
+        let db = testdb().await;
+
+        // Create and commit some data to get a historical revision
+        let batch = vec![BatchOp::Put {
+            key: b"historical_key",
+            value: b"historical_value",
+        }];
+        let proposal = db.propose(batch).await.unwrap();
+        let historical_hash = proposal.root_hash().await.unwrap().unwrap();
+        proposal.commit().await.unwrap();
+
+        // Create a new proposal (uncommitted)
+        let batch = vec![BatchOp::Put {
+            key: b"proposal_key",
+            value: b"proposal_value",
+        }];
+        let proposal = db.propose(batch).await.unwrap();
+        let proposal_hash = proposal.root_hash().await.unwrap().unwrap();
+
+        // Test that view_sync can find the historical revision
+        let historical_view = db.view_sync(historical_hash).unwrap();
+        let value = historical_view
+            .val_sync_bytes(b"historical_key")
+            .unwrap()
+            .unwrap();
+        assert_eq!(&*value, b"historical_value");
+
+        // Test that view_sync can find the proposal
+        let proposal_view = db.view_sync(proposal_hash).unwrap();
+        let value = proposal_view
+            .val_sync_bytes(b"proposal_key")
+            .unwrap()
+            .unwrap();
+        assert_eq!(&*value, b"proposal_value");
+
+        // Clean up - commit the proposal to avoid it being dropped
+        proposal.commit().await.unwrap();
     }
 
     // Testdb is a helper struct for testing the Db. Once it's dropped, the directory and file disappear
