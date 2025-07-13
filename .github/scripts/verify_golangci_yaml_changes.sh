468839b8ff79e9d93cebbba1c5a513a1cffada24
Rodrigo Villar <rodrigo.villar@avalabs.org>
2025-07-13 10:34:58 -0400

    chore: move CI logic to .github

--- .github/scripts/verify_golangci_yaml_changes.sh
+++ .github/scripts/verify_golangci_yaml_changes.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 curl -o /tmp/upstream.yml https://raw.githubusercontent.com/ava-labs/avalanchego/refs/heads/master/.golangci.yml
-    
+
+if ! [[ "$0" =~ scripts/verify_golangci_yaml_changes.sh ]]; then
+  echo "must be run from .github"
+  exit 255
+fi
+
 # Generate diff
-diff /tmp/upstream.yml .golangci.yaml > /tmp/diff.txt || true
+diff /tmp/upstream.yml ../ffi/.golangci.yaml > /tmp/diff.txt || true
 
 # Compare with expected diff
 if ! diff /tmp/diff.txt .golangci_yaml_expected_changes.txt; then
-    echo ".golangci.yaml has unexpected changes from AvalancheGo"
+    echo "ffi/.golangci.yaml has unexpected changes from AvalancheGo"
     exit 1
 fi
