d1f7cdad3d98b6efcd0e74295e41fae2bbdf31f9
qusuyan <qusuyan@gmail.com>
2025-07-24 17:11:26 -0400

    reorganize hash_helper

--- storage/src/nodestore/hash.rs
+++ storage/src/nodestore/hash.rs
@@ -77,55 +77,50 @@ where
         #[cfg(feature = "ethhash")] &self,
         mut node: Node,
         path_prefix: &mut Path,
-        #[cfg(feature = "ethhash")] fake_root_extra_nibble: Option<u8>,
+        #[cfg(feature = "ethhash")] account_child_has_peers: bool,
     ) -> Result<(MaybePersistedNode, HashType), FileIoError> {
         // If this is a branch, find all unhashed children and recursively hash them.
         trace!("hashing {node:?} at {path_prefix:?}");
         if let Node::Branch(ref mut b) = node {
-            // special case code for ethereum hashes at the account level
             #[cfg(feature = "ethhash")]
-            let make_fake_root = if path_prefix.0.len().saturating_add(b.partial_path.0.len()) == 64
-            {
-                // looks like we're at an account branch
-                // tally up how many hashes we need to deal with
-                let ClassifiedChildren {
-                    unhashed,
-                    mut hashed,
-                } = self.ethhash_classify_children(&mut b.children);
-                trace!("hashed {hashed:?} unhashed {unhashed:?}");
-                if hashed.len() == 1 {
-                    // we were left with one hashed node that must be rehashed
-                    let (invalidated_node_idx, (invalidated_node, invalidated_hash)) =
-                        hashed.first_mut().expect("hashed is not empty");
-                    // Extract the address from the MaybePersistedNode
-                    let addr: crate::LinearAddress = invalidated_node
-                        .as_linear_address()
-                        .expect("hashed node should be persisted");
-                    let mut hashable_node = self.read_node(addr)?.deref().clone();
-                    let original_length = path_prefix.len();
-                    path_prefix.0.extend(b.partial_path.0.iter().copied());
-                    if unhashed.is_empty() {
-                        hashable_node.update_partial_path(Path::from_nibbles_iterator(
-                            std::iter::once(*invalidated_node_idx as u8)
-                                .chain(hashable_node.partial_path().0.iter().copied()),
-                        ));
-                    } else {
-                        path_prefix.0.push(*invalidated_node_idx as u8);
+            // special case code for ethhash at the account level
+            let have_multiple_children =
+                if path_prefix.0.len().saturating_add(b.partial_path.0.len()) == 64 {
+                    let ClassifiedChildren {
+                        unhashed,
+                        mut hashed,
+                    } = self.ethhash_classify_children(&mut b.children);
+                    trace!("hashed {hashed:?} unhashed {unhashed:?}");
+                    if hashed.len() == 1 && !unhashed.is_empty() {
+                        // special case:
+                        //  - there was only one child in the current account branch when previously hashed
+                        //  - but now we are adding more children
+                        // we need to rehash the child
+                        let (child_idx, (child_node, child_hash)) =
+                            hashed.first_mut().expect("hashed is not empty");
+                        let addr: crate::LinearAddress = child_node
+                            .as_linear_address()
+                            .expect("hashed node should be persisted");
+                        let hashable_node = self.read_node(addr)?.deref().clone();
+                        let original_length = path_prefix.len();
+                        path_prefix.0.extend(b.partial_path.0.iter().copied());
+                        path_prefix.0.push(*child_idx as u8);
+                        let hash = Self::compute_node_ethhash(&hashable_node, path_prefix, true);
+                        path_prefix.0.truncate(original_length);
+                        **child_hash = hash;
                     }
-                    let hash = hash_node(&hashable_node, path_prefix);
-                    path_prefix.0.truncate(original_length);
-                    **invalidated_hash = hash;
-                }
-                // handle the single-child case for an account special below
-                if hashed.is_empty() && unhashed.len() == 1 {
-                    Some(unhashed.last().expect("only one").0 as u8)
+
+                    {
+                        #![expect(
+                            clippy::arithmetic_side_effects,
+                            reason = "hashed and unhashed can have at most 16 elements"
+                        )]
+                        hashed.len() + unhashed.len() > 1
+                    }
                 } else {
-                    None
-                }
-            } else {
-                // not a single child
-                None
-            };
+                    // not an account branch
+                    false
+                };
 
             // branch children cases:
             // 1. 1 child, already hashed
@@ -135,6 +130,7 @@ where
             // 5. 1 hashed, >0 unhashed <-- rehash case
             // 6. everything already hashed
 
+            // general case: recusively hash unhashed children
             for (nibble, child) in b.children.iter_mut().enumerate() {
                 // If this is empty or already hashed, we're done
                 // Empty matches None, and non-Node types match Some(None) here, so we want
@@ -150,18 +146,11 @@ where
                 // we extend and truncate path_prefix to reduce memory allocations
                 let original_length = path_prefix.len();
                 path_prefix.0.extend(b.partial_path.0.iter().copied());
-                #[cfg(feature = "ethhash")]
-                if make_fake_root.is_none() {
-                    // we don't push the nibble there is only one unhashed child and
-                    // we're on an account
-                    path_prefix.0.push(nibble as u8);
-                }
-                #[cfg(not(feature = "ethhash"))]
                 path_prefix.0.push(nibble as u8);
 
                 #[cfg(feature = "ethhash")]
                 let (child_node, child_hash) =
-                    self.hash_helper(child_node, path_prefix, make_fake_root)?;
+                    self.hash_helper(child_node, path_prefix, have_multiple_children)?;
                 #[cfg(not(feature = "ethhash"))]
                 let (child_node, child_hash) = Self::hash_helper(child_node, path_prefix)?;
 
@@ -170,24 +159,11 @@ where
                 path_prefix.0.truncate(original_length);
             }
         }
+
         // At this point, we either have a leaf or a branch with all children hashed.
         // if the encoded child hash <32 bytes then we use that RLP
-
         #[cfg(feature = "ethhash")]
-        // if we have a child that is the only child of an account branch, we will hash this child as if it
-        // is a root node. This means we have to take the nibble from the parent and prefix it to the partial path
-        let hash = if let Some(nibble) = fake_root_extra_nibble {
-            let mut fake_root = node.clone();
-            trace!("old node: {fake_root:?}");
-            fake_root.update_partial_path(Path::from_nibbles_iterator(
-                std::iter::once(nibble).chain(fake_root.partial_path().0.iter().copied()),
-            ));
-            trace!("new node: {fake_root:?}");
-            hash_node(&fake_root, path_prefix)
-        } else {
-            hash_node(&node, path_prefix)
-        };
-
+        let hash = Self::compute_node_ethhash(&node, path_prefix, account_child_has_peers);
         #[cfg(not(feature = "ethhash"))]
         let hash = hash_node(&node, path_prefix);
 
@@ -198,9 +174,9 @@ where
     pub(crate) fn compute_node_ethhash(
         node: &Node,
         path_prefix: &Path,
-        have_peers: bool,
+        account_child_has_peers: bool,
     ) -> HashType {
-        if path_prefix.0.len() == 65 && !have_peers {
+        if path_prefix.0.len() == 65 && !account_child_has_peers {
             // This is the special case when this node is the only child of an account
             //  - 64 nibbles for account + 1 nibble for its position in account branch node
             let mut fake_root = node.clone();
