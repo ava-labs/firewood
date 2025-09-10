6b33cf2809d756789628b8fdbb4d293d83563fd2
Brandon LeBlanc <brandon.leblanc@avalabs.org>
2025-09-10 13:21:06 -0700

    simplify tests

--- firewood/src/proofs/marshaling.rs
+++ firewood/src/proofs/marshaling.rs
@@ -195,7 +195,7 @@ impl std::fmt::Debug for ChildrenMap {
     }
 }
 
-#[cfg(all(test, not(feature = "branch_factor_256")))]
+#[cfg(test)]
 mod tests {
     use super::*;
 
@@ -203,39 +203,37 @@ mod tests {
     use test_case::test_case;
 
     #[test_case(BranchNode::empty_children(), &[]; "empty")]
-    #[test_case([Some(()), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None], &[0]; "first")]
-    #[test_case([None, Some(()), None, None, None, None, None, None, None, None, None, None, None, None, None, None], &[1]; "second")]
-    #[test_case([None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Some(())], &[15]; "last")]
-    #[test_case([Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None], &[0, 2, 4, 6, 8, 10, 12, 14]; "evens")]
-    #[test_case([None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(())], &[1, 3, 5, 7, 9, 11, 13, 15]; "odds")]
-    #[test_case([Some(()); 16], &(0..16).collect::<Vec<_>>(); "all")]
-    fn test_children_map(children: Children<()>, indicies: &[usize]) {
-        let map = ChildrenMap::new(&children);
-        assert_eq!(map.len(), indicies.len());
-
-        assert!(
-            indicies.iter().copied().eq(map.iter_indices()),
-            "expected {:?}, got {:?}",
-            indicies,
-            map.iter_indices().collect::<Vec<_>>()
-        );
-    }
-}
-
-#[cfg(all(test, feature = "branch_factor_256"))]
-mod tests {
-    use super::*;
-
-    use firewood_storage::BranchNode;
-    use test_case::test_case;
-
-    #[test_case(BranchNode::empty_children(), &[]; "empty")]
-    #[test_case([Some(()), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None], &[0]; "first")]
-    #[test_case([None, Some(()), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None], &[1]; "second")]
-    #[test_case([None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Some(())], &[255]; "last")]
-    #[test_case([Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None], &[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 254]; "evens")]
-    #[test_case([None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(()), None, Some(())], &[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 115, 117, 119, 121, 123, 125, 127, 129, 131, 133, 135, 137, 139, 141, 143, 145, 147, 149, 151, 153, 155, 157, 159, 161, 163, 165, 167, 169, 171, 173, 175, 177, 179, 181, 183, 185, 187, 189, 191, 193, 195, 197, 199, 201, 203, 205, 207, 209, 211, 213, 215, 217, 219, 221, 223, 225, 227, 229, 231, 233, 235, 237, 239, 241, 243, 245, 247, 249, 251, 253, 255]; "odds")]
-    #[test_case([Some(()); 256], &(0..256).collect::<Vec<_>>(); "all")]
+    #[test_case(
+        {
+            let mut children = BranchNode::empty_children();
+            children[0] = Some(());
+            children
+        }, &[0]; "first")]
+    #[test_case({
+        let mut children = BranchNode::empty_children();
+        children[1] = Some(());
+        children
+    }, &[1]; "second")]
+    #[test_case({
+        let mut children = BranchNode::empty_children();
+        children[BranchNode::MAX_CHILDREN - 1] = Some(());
+        children
+    }, &[BranchNode::MAX_CHILDREN - 1]; "last")]
+    #[test_case({
+        let mut children = BranchNode::empty_children();
+        for slot in children.iter_mut().step_by(2) {
+            *slot = Some(());
+        }
+        children
+    }, &(0..BranchNode::MAX_CHILDREN).step_by(2).collect::<Vec<_>>(); "evens")]
+    #[test_case({
+        let mut children = BranchNode::empty_children();
+        for slot in children.iter_mut().skip(1).step_by(2) {
+            *slot = Some(());
+        }
+        children
+    }, &(1..BranchNode::MAX_CHILDREN).step_by(2).collect::<Vec<_>>(); "odds")]
+    #[test_case([Some(()); BranchNode::MAX_CHILDREN], &(0..BranchNode::MAX_CHILDREN).collect::<Vec<_>>(); "all")]
     fn test_children_map(children: Children<()>, indicies: &[usize]) {
         let map = ChildrenMap::new(&children);
         assert_eq!(map.len(), indicies.len());
