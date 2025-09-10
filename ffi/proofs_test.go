49f9366c398158c5246261d3a21627ff493dbdd2
Brandon LeBlanc <brandon.leblanc@avalabs.org>
2025-09-09 19:45:52 -0700

    ensure partial range proofs generate and serialize

--- ffi/proofs_test.go
+++ ffi/proofs_test.go
@@ -22,6 +22,66 @@ func (m maybe) Value() []byte {
 	return m.value
 }
 
+func TestPartialRangeProof(t *testing.T) {
+	r := require.New(t)
+	db := newTestDatabase(t)
+
+	// check empty database
+	proof, err := db.RangeProof(nil, nil, nil, 0)
+	r.ErrorIs(err, errEmptyTrie)
+	r.Nil(proof)
+
+	// Insert a lot of data.
+	keys, vals := kvForTest(10000)
+	root, err := db.Update(keys, vals)
+	r.NoError(err)
+
+	proof, err = db.RangeProof(nil, nil, nil, 10)
+	r.NoError(err)
+	r.NotNil(proof)
+	proof1Bytes, err := proof.MarshalBinary()
+	r.NoError(err)
+	r.NoError(proof.Free())
+
+	proof, err = db.RangeProof(maybe{true, root}, nil, nil, 10)
+	r.NoError(err)
+	r.NotNil(proof)
+	proof2Bytes, err := proof.MarshalBinary()
+	r.NoError(err)
+	r.NoError(proof.Free())
+
+	r.Equal(proof1Bytes, proof2Bytes)
+
+	// Deserialize the proof.
+	proof3 := new(RangeProof)
+	err = proof3.UnmarshalBinary(proof1Bytes)
+	r.NoError(err)
+	r.NoError(proof3.Free())
+
+	proof, err = db.RangeProof(nil, maybe{true, []byte("key2")}, maybe{true, []byte("key3")}, 10)
+	r.NoError(err)
+	r.NotNil(proof)
+	proof4Bytes, err := proof.MarshalBinary()
+	r.NoError(err)
+	r.NoError(proof.Free())
+
+	proof, err = db.RangeProof(maybe{true, root}, maybe{true, []byte("key2")}, maybe{true, []byte("key3")}, 10)
+	r.NoError(err)
+	r.NotNil(proof)
+	proof5Bytes, err := proof.MarshalBinary()
+	r.NoError(err)
+	r.NoError(proof.Free())
+
+	r.Equal(proof4Bytes, proof5Bytes)
+	r.NotEqual(proof1Bytes, proof4Bytes)
+
+	// Deserialize the proof.
+	proof6 := new(RangeProof)
+	err = proof6.UnmarshalBinary(proof4Bytes)
+	r.NoError(err)
+	r.NoError(proof6.Free())
+}
+
 func TestRoundTripSerialization(t *testing.T) {
 	r := require.New(t)
 	db := newTestDatabase(t)
