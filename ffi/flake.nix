92d7781a3345738d3f22e8c6cb8f02765e212e4b
maru <maru.newby@avalabs.org>
2025-09-30 06:28:46 +0000

    fixup: Add crane for dep artifact caching and update static lib action

--- ffi/flake.nix
+++ ffi/flake.nix
@@ -4,96 +4,110 @@
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2505.*.tar.gz";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs = { self, nixpkgs, rust-overlay, crane, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      inherit (pkgs) lib;
+
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rustfmt" "clippy" ];
+      };
+
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+      # Extract crate info from Cargo.toml files
+      ffiCargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      workspaceCargoToml = builtins.fromTOML (builtins.readFile ../Cargo.toml);
+
+      src = lib.cleanSourceWith {
+        src = craneLib.path ./..;
+        filter = path: type:
+          (lib.hasSuffix "\.md" path) ||
+          (craneLib.filterCargoSources path type);
+      };
+
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+
+        # Build only the firewood-ffi crate
+        pname = ffiCargoToml.package.name;
+        version = workspaceCargoToml.workspace.package.version;
+
+        CARGO_PROFILE = "maxperf";
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ] ++ lib.optionals stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.Security
+          darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
+      };
+
+      cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
+        cargoExtraArgs = "--package ${ffiCargoToml.package.name} --features ethhash,logger";
+      });
+
+      firewood-ffi = craneLib.buildPackage (commonArgs // {
+        inherit cargoArtifacts;
+        cargoExtraArgs = "--package ${ffiCargoToml.package.name} --features ethhash,logger --frozen";
+
+        # Install the static library and header
+        postInstall = ''
+          mkdir -p $out/lib $out/include
+          cp target/*/libfirewood_ffi.a $out/lib/ || cp target/release/libfirewood_ffi.a $out/lib/
+          cp ffi/firewood.h $out/include/
+        '';
+
+        meta = with lib; {
+          description = "C FFI bindings for Firewood, an embedded key-value store";
+          homepage = "https://github.com/ava-labs/firewood";
+          license = {
+            fullName = "Ava Labs Ecosystem License 1.1";
+            url = "https://github.com/ava-labs/firewood/blob/main/LICENSE.md";
+          };
+          platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+        };
+      } // lib.optionalAttrs pkgs.stdenv.isDarwin {
+        # Set macOS deployment target for Darwin builds
+        MACOSX_DEPLOYMENT_TARGET = "13.0";
+      });
     in
     {
-      packages = forAllSystems (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs { inherit system overlays; };
-          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" "rustfmt" "clippy" ];
-          };
-        in
-        {
-          firewood-ffi = pkgs.rustPlatform.buildRustPackage {
-            pname = "firewood-ffi";
-            version = "0.0.12";
+      packages = {
+        inherit firewood-ffi;
+        default = firewood-ffi;
+      };
 
-            src = ./..;
+      devShells.default = craneLib.devShell {
+        inputsFrom = [ firewood-ffi ];
 
-            cargoLock = {
-              lockFile = ../Cargo.lock;
-            };
+        packages = with pkgs; [
+          rustToolchain
+          go
+        ];
 
-            # Build only the firewood-ffi crate
-            cargoBuildFlags = [ "--package" "firewood-ffi" ];
-            cargoTestFlags = [ "--package" "firewood-ffi" ];
+        shellHook = ''
+          echo "Firewood FFI development environment"
+          echo "Use 'nix build .#firewood-ffi' to build the FFI package"
 
-            nativeBuildInputs = with pkgs; [
-              rustToolchain
-              pkg-config
-            ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-              pkgs.darwin.apple_sdk.frameworks.Security
-              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-            ];
+          # Set up CGO environment
+          export CGO_LDFLAGS="-L${firewood-ffi}/lib -lfirewood_ffi -lm"
+          export CGO_CFLAGS="-I${firewood-ffi}/include"
 
-            buildInputs = with pkgs; [
-              gcc
-            ];
-
-            # Install the static library and header
-            postInstall = ''
-              mkdir -p $out/lib $out/include
-              cp target/*/libfirewood_ffi.a $out/lib/ || cp target/release/libfirewood_ffi.a $out/lib/
-              cp ffi/firewood.h $out/include/
-            '';
-
-            meta = with pkgs.lib; {
-              description = "C FFI bindings for Firewood, an embedded key-value store";
-              homepage = "https://github.com/ava-labs/firewood";
-              license = licenses.free;
-              platforms = platforms.all;
-            };
-          };
-
-
-          default = self.packages.${system}.firewood-ffi;
-        });
-
-      devShells = forAllSystems (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs { inherit system overlays; };
-          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" "rustfmt" "clippy" ];
-          };
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              rustToolchain
-              pkg-config
-              gcc
-            ] ++ pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [
-              # Linux-specific packages
-            ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-              # Darwin-specific packages
-              pkgs.darwin.apple_sdk.frameworks.Security
-              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-            ];
-
-            shellHook = ''
-              echo "Firewood FFI development environment"
-              echo "Use 'nix build .#firewood-ffi' to build the FFI package"
-              echo "Built package will have headers in include/ and library in lib/"
-            '';
-          };
-        });
-    };
+          # TODO(marun) Maybe add a script?
+          echo ""
+          echo "To run Go tests:"
+          echo "  export GOEXPERIMENT=cgocheck2"
+          echo "  export TEST_FIREWOOD_HASH_MODE=ethhash"
+          echo "  go test ./... -v"
+        '';
+      };
+    });
 }
