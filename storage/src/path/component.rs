baf565e2b3e4b15d656b62ad3d8f581941ca3148
Brandon LeBlanc <brandon.leblanc@avalabs.org>
2025-10-08 09:58:18 -0700

    add some comments

--- storage/src/path/component.rs
+++ storage/src/path/component.rs
@@ -293,6 +293,9 @@ impl super::TriePathFromPackedBytes<'_> for Vec<PathComponent> {
     fn path_from_packed_bytes(bytes: &'_ [u8]) -> Self {
         let path = super::PackedPathRef::path_from_packed_bytes(bytes);
         let mut this = Vec::new();
+        // reserve_exact is used because we trust that `TriePath::len` returns the exact
+        // length but `Vec::extend` won't trust `Iterator::size_hint` and may
+        // over/under-allocate.
         this.reserve_exact(path.len());
         this.extend(path.components());
         this
@@ -305,6 +308,9 @@ impl<A: smallvec::Array<Item = PathComponent>> super::TriePathFromPackedBytes<'_
         let path = super::PackedPathRef::path_from_packed_bytes(bytes);
         let mut this = SmallVec::<A>::new();
         this.reserve_exact(path.len());
+        // reserve_exact is used because we trust that `TriePath::len` returns the exact
+        // length but `SmallVec::extend` won't trust `Iterator::size_hint` and may
+        // over/under-allocate.
         this.extend(path.components());
         this
     }
