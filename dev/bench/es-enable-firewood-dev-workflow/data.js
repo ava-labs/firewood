fb1c7befca329b4ccfe37faf719a40f4a9c2d4f5
github-action-benchmark <github@users.noreply.github.com>
2025-12-02 18:53:47 +0000

    add C-Chain Reexecution Performance (go) benchmark result for 4aa0e08e786703d759b3698c474f3cf9eff87a8c

---
+++ dev/bench/es-enable-firewood-dev-workflow/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1764701627881,
+  "repoUrl": "https://github.com/ava-labs/firewood",
+  "entries": {
+    "C-Chain Reexecution Performance": [
+      {
+        "commit": {
+          "author": {
+            "name": "Elvis S.",
+            "username": "Elvis339",
+            "email": "elvissabanovic3@gmail.com"
+          },
+          "committer": {
+            "name": "Elvis S.",
+            "username": "Elvis339",
+            "email": "elvissabanovic3@gmail.com"
+          },
+          "id": "4aa0e08e786703d759b3698c474f3cf9eff87a8c",
+          "message": "ci: use switch CI token",
+          "timestamp": "2025-12-02T18:13:18Z",
+          "url": "https://github.com/ava-labs/firewood/commit/4aa0e08e786703d759b3698c474f3cf9eff87a8c"
+        },
+        "date": 1764701627270,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[101,250000]-Config-firewood-Runner-avalanche-avalanchego-runner-2ti-z284t-runner-8lq2r",
+            "value": 87.73,
+            "unit": "mgas/s",
+            "extra": "1 times\n32 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
