cd1e878ef6f25c549c18c1b4304cacbf46d2faf3
Bernard Wong <bernard@avalabs.org>
2025-12-20 15:35:47 -0500

    Addressing some of the feedback

--- firewood/src/merkle/changes.rs
+++ firewood/src/merkle/changes.rs
@@ -13,27 +13,28 @@ use crate::{
     merkle::{Key, PrefixOverlap},
 };
 
-/// Contains all of a node's state that is needed for node comparison in `DiffMerkleNodeStream`.
+/// Contains all of a node's info that is needed for node comparison in `DiffMerkleNodeStream`.
 /// It includes the nodes full path and its hash if available.
 #[cfg_attr(not(test), expect(dead_code))]
 #[derive(Clone)]
-struct NodeState {
+struct ComparableNodeInfo {
     path: Path,
     node: SharedNode,
     hash: Option<TrieHash>,
 }
 
-impl NodeState {
-    // Creates a NodeState from a `Child`, its pre-path, and trie for reading the node from storage.
+impl ComparableNodeInfo {
+    // Creates a `ComparableNodeInfo` from a `Child`, its pre-path, and the trie that the `Child`
+    // is from for reading the node from storage.
     fn new<T: NodeReader>(pre_path: Path, child: &Child, trie: &T) -> Result<Self, FileIoError> {
         let node = child.as_shared_node(trie)?;
-        // Create the full path for the node. This is used to compare with the full path
-        // of the current node from the other trie.
-        let mut path = pre_path;
-        path.extend(node.partial_path().iter().copied());
+        // We need the full path as the diff algorithm compares the full paths of the current
+        // nodes of the two tries.
+        let mut full_path = pre_path;
+        full_path.extend(node.partial_path().iter().copied());
 
         Ok(Self {
-            path,
+            path: full_path,
             node,
             hash: child.hash().map(|hash| hash.clone().into_triehash()),
         })
@@ -41,12 +42,13 @@ impl NodeState {
 }
 
 /// Contains the state required for performing pre-order iteration of a Merkle trie. This includes
-/// the state of the current node, a reference to a trie that implements the `HashedNodeReader`
-/// trait, and a stack that contains the `NodeState` of nodes to be traversed with calls to `next`.
+/// the `ComparableNodeInfo` of the current node, a reference to a trie that implements the
+/// `HashedNodeReader` trait, and a stack that contains the `ComparableNodeInfo` of nodes to be
+/// traversed with calls to `next`.
 struct PreOrderIterator<'a, T: HashedNodeReader> {
-    node_state: Option<NodeState>,
+    node_info: Option<ComparableNodeInfo>,
     trie: &'a T,
-    traversal_stack: Vec<NodeState>,
+    traversal_stack: Vec<ComparableNodeInfo>,
 }
 
 impl<'a, T: HashedNodeReader> PreOrderIterator<'a, T> {
@@ -54,17 +56,18 @@ impl<'a, T: HashedNodeReader> PreOrderIterator<'a, T> {
     #[cfg_attr(not(test), expect(dead_code))]
     fn new(trie: &'a T, start_key: &Key) -> Result<PreOrderIterator<'a, T>, FileIoError> {
         let mut preorder = Self {
-            node_state: None,
+            node_info: None,
             traversal_stack: vec![],
             trie,
         };
-        // If the root node is not None, then push a `NodeState` for the root onto the traversal
-        // stack. It will be used on the first call to `next` or `next_internal`. Because we
-        // already have the root node, we create its `NodeState` directly instead of using
-        // `NodeState::new` as we don't need to create a `SharedNode` from a `Child`. The full
-        // path of the root node is just its partial path since it has no pre-path.
+        // If the root node is not None, then push a `ComparableNodeInfo` for the root onto
+        // the traversal stack. It will be used on the first call to `next` or `next_internal`.
+        // Because we already have the root node, we create its `ComparableNodeInfo` directly
+        // instead of using `ComparableNodeInfo::new` as we don't need to create a `SharedNode`
+        // from a `Child`. The full path of the root node is just its partial path since it has
+        // no pre-path.
         if let Some(root) = trie.root_node() {
-            preorder.traversal_stack.push(NodeState {
+            preorder.traversal_stack.push(ComparableNodeInfo {
                 path: root.partial_path().clone(),
                 node: root,
                 hash: trie.root_hash().map(|hash| hash.clone().into_triehash()),
@@ -81,51 +84,54 @@ impl<'a, T: HashedNodeReader> PreOrderIterator<'a, T> {
     /// stack.
     ///
     /// This implementation flips the order such that we don't push a node's children onto the
-    /// traversal stack until the subseqent call to `next`. This is done by saving the node state of
-    /// the current node in `node_state` and keeping that available for the next call. Skipping
-    /// traversal of the children then just involves setting `node_state` to None.
+    /// traversal stack until the subseqent call to `next`. This is done by saving the
+    /// `ComparableNodeInfo` of the current node in `node_info` and keeping that available for the
+    /// next call. Skipping traversal of the children then just involves setting `node_info` to None.
     #[cfg_attr(not(test), expect(dead_code))]
-    fn next(&mut self) -> Result<Option<&NodeState>, FileIoError> {
+    fn next(&mut self) -> Result<Option<&ComparableNodeInfo>, FileIoError> {
         firewood_counter!(
             "firewood.change_proof.next",
-            "number of next calls on PreOrderIterator",
-            "traversal" => "next")
+            "number of next calls to calculate a change proof"
+        )
         .increment(1);
 
-        // Take the state of the current node (which will soon be replaced), and check if it is a
+        // Take the info of the current node (which will soon be replaced), and check if it is a
         // branch. If it is, add its children onto the traversal stack.
-        if let Some(prev_node_state) = self.node_state.take()
-            && let Node::Branch(branch) = &*prev_node_state.node
+        if let Some(prev_node_info) = self.node_info.take()
+            && let Node::Branch(branch) = &*prev_node_info.node
         {
             // Since a stack is LIFO and we want to perform pre-order traversal, we pushed the
             // children in reverse order.
             for (path_comp, child) in branch.children.iter_present().rev() {
                 // Generate the pre-path for this child, and push it onto the traversal stack.
                 let child_pre_path = Path::from_nibbles_iterator(
-                    prev_node_state
+                    prev_node_info
                         .path
                         .iter()
                         .copied()
                         .chain(once(path_comp.as_u8())),
                 );
-                self.traversal_stack
-                    .push(NodeState::new(child_pre_path, child, self.trie)?);
+                self.traversal_stack.push(ComparableNodeInfo::new(
+                    child_pre_path,
+                    child,
+                    self.trie,
+                )?);
             }
         }
 
-        // Get the next node from the traversal stack and set it as the current node state.
-        // If the stack is empty, then return None and the iteration is complete. Otherwise
-        // return a reference to the current node state.
-        self.node_state = self.traversal_stack.pop();
-        Ok(self.node_state.as_ref())
+        // Pop a `ComparableNodeInfo` from the traversal stack and set it as the current node's
+        // info. If the stack is empty, then return None and the iteration is complete. Otherwise
+        // return a reference to the current node's info.
+        self.node_info = self.traversal_stack.pop();
+        Ok(self.node_info.as_ref())
     }
 
-    /// Calling `skip_children` will clear the current node state, which will cause the children
+    /// Calling `skip_children` will clear the current node's info, which will cause the children
     /// of the current node to not be added to the traversal stack on the subsequent `next` call.
     /// This will effectively cause the traversal to skip the children of the current node.
     #[cfg_attr(not(test), expect(dead_code))]
     fn skip_children(&mut self) {
-        self.node_state = None;
+        self.node_info = None;
     }
 
     /// Modify the iterator to skip all keys prior to the specified key.
@@ -139,8 +145,8 @@ impl<'a, T: HashedNodeReader> PreOrderIterator<'a, T> {
         loop {
             // Push the children that can contain the keys that are larger than or equal to the
             // the start key onto the traversal stack.
-            if let Some(prev_node_state) = self.node_state.take()
-                && let Node::Branch(branch) = &*prev_node_state.node
+            if let Some(prev_node_info) = self.node_info.take()
+                && let Node::Branch(branch) = &*prev_node_info.node
             {
                 // Create a reverse iterator on the children that includes the child's pre-path.
                 let mut reversed_children_with_pre_path =
@@ -152,7 +158,7 @@ impl<'a, T: HashedNodeReader> PreOrderIterator<'a, T> {
                             (
                                 child,
                                 Path::from_nibbles_iterator(
-                                    prev_node_state
+                                    prev_node_info
                                         .path
                                         .iter()
                                         .copied()
@@ -168,7 +174,7 @@ impl<'a, T: HashedNodeReader> PreOrderIterator<'a, T> {
                     let path_overlap = PrefixOverlap::from(key, child_key.as_ref());
                     let unique_node = path_overlap.unique_b;
                     if unique_node.is_empty() || child_key > *key {
-                        self.traversal_stack.push(NodeState::new(
+                        self.traversal_stack.push(ComparableNodeInfo::new(
                             child_pre_path,
                             child,
                             self.trie,
@@ -180,35 +186,35 @@ impl<'a, T: HashedNodeReader> PreOrderIterator<'a, T> {
                 }
 
                 // Add the rest of the children (if any) to the traversal stack.
-                for node_state in reversed_children_with_pre_path
-                    .map(|(child, child_pre_path)| NodeState::new(child_pre_path, child, self.trie))
-                {
-                    self.traversal_stack.push(node_state?);
+                for node_info in reversed_children_with_pre_path.map(|(child, child_pre_path)| {
+                    ComparableNodeInfo::new(child_pre_path, child, self.trie)
+                }) {
+                    self.traversal_stack.push(node_info?);
                 }
             }
 
-            // Pop the next node state from the stack
-            if let Some(state) = self.traversal_stack.pop() {
+            // Pop the next node's info from the stack
+            if let Some(node_info) = self.traversal_stack.pop() {
                 // Since pre-order traversal of a trie iterates through the nodes in lexicographical
                 // order, we can stop the traversal once we see a node key that is larger than or
-                // equal to the key. We stop the traversal by pushing the current `NodeState`
+                // equal to the key. We stop the traversal by pushing the current `ComparableNodeInfo`
                 // back to the stack. Calling `next` will process this node.
-                let node_key = key_from_nibble_iter(state.path.iter().copied());
+                let node_key = key_from_nibble_iter(node_info.path.iter().copied());
                 if node_key >= *key {
-                    self.traversal_stack.push(state);
+                    self.traversal_stack.push(node_info);
                     return Ok(());
                 }
 
-                // If this node is a leaf, then we don't need to save its state to `node_state` as
+                // If this node is a leaf, then we don't need to save its info to `node_info` as
                 // it has no children to traverse.
-                if let Node::Branch(_branch) = &*state.node {
+                if let Node::Branch(_branch) = &*node_info.node {
                     // Check if this node's path is a prefix of the key. If it is not (`unique_node`
                     // is not empty), then this node's children cannot be larger than or equal to
                     // the key, and we don't need to include them on the traversal stack.
                     let path_overlap = PrefixOverlap::from(key, node_key.as_ref());
                     let unique_node = path_overlap.unique_b;
                     if unique_node.is_empty() {
-                        self.node_state = Some(state);
+                        self.node_info = Some(node_info);
                     }
                 }
             } else {
@@ -340,10 +346,10 @@ mod tests {
         // Check if the sorted batch and the pre-order traversal have identical values.
         let mut preorder_it = PreOrderIterator::new(merkle.nodestore(), &Key::default()).unwrap();
         let mut batch_sorted_it = batch_sorted.clone().into_iter();
-        while let Some(node_state) = preorder_it.next().unwrap() {
-            assert!(node_state.hash.is_some());
-            if let Some(val) = node_state.node.value() {
-                let key = key_from_nibble_iter(node_state.path.iter().copied());
+        while let Some(node_info) = preorder_it.next().unwrap() {
+            assert!(node_info.hash.is_some());
+            if let Some(val) = node_info.node.value() {
+                let key = key_from_nibble_iter(node_info.path.iter().copied());
                 let batch_sorted_item = batch_sorted_it.next().unwrap();
                 assert!(
                     *key == *batch_sorted_item.key().as_slice()
@@ -363,9 +369,9 @@ mod tests {
             .clone()
             .into_boxed_slice();
         let mut preorder_it = PreOrderIterator::new(merkle.nodestore(), &start_key).unwrap();
-        while let Some(node_state) = preorder_it.next().unwrap() {
-            if let Some(val) = node_state.node.value() {
-                let key = key_from_nibble_iter(node_state.path.iter().copied());
+        while let Some(node_info) = preorder_it.next().unwrap() {
+            if let Some(val) = node_info.node.value() {
+                let key = key_from_nibble_iter(node_info.path.iter().copied());
                 let batch_sorted_item = batch_sorted.get(index).unwrap();
                 assert!(
                     *key == *batch_sorted_item.key().as_slice()
