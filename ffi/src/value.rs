840350e926119a2978534d62635d4e328619018e
Bernard Wong <bernard@avalabs.org>
2026-02-11 01:00:08 -0500

    Adding visibility to new result structs

--- ffi/src/value.rs
+++ ffi/src/value.rs
@@ -16,8 +16,9 @@ pub use self::owned::{OwnedBytes, OwnedSlice};
 pub(crate) use self::results::{CResult, NullHandleResult};
 pub use self::results::{
     ChangeProofResult, CodeIteratorResult, HandleResult, HashResult, IteratorResult,
-    KeyValueBatchResult, KeyValueResult, NextKeyRangeResult, ProposalResult, RangeProofResult,
-    RevisionResult, ValueResult, VoidResult,
+    KeyValueBatchResult, KeyValueResult, NextKeyRangeResult, ProposalResult,
+    ProposedChangeProofResult, RangeProofResult, RevisionResult, ValueResult,
+    VerifiedChangeProofResult, VoidResult,
 };
 
 /// Maybe is a C-compatible optional type using a tagged union pattern.
