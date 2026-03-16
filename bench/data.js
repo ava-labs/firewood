57499d3d05f670407444a8250235d037a5aa256c
github-action-benchmark <github@users.noreply.github.com>
2026-03-16 07:49:51 +0000

    add C-Chain Reexecution with Firewood (customBiggerIsBetter) benchmark result for 5406b6858e002d783b5a9c32db9b49fd18291c0b

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773387588950,
+  "lastUpdate": 1773647391330,
   "repoUrl": "https://github.com/ava-labs/firewood",
   "entries": {
     "C-Chain Reexecution with Firewood": [
@@ -1409,6 +1409,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[40000001,41000000]-Config-firewood-Runner-avago-runner-i4i-2xlarge-local-ssd - block_accept_ms/ggas",
             "value": 81.99601538134611,
+            "unit": "block_accept_ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Elvis",
+            "username": "Elvis339",
+            "email": "43846394+Elvis339@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "5406b6858e002d783b5a9c32db9b49fd18291c0b",
+          "message": "chore(revert): remove nix flake and all references to it (#1788) (#1794)\n\n## Why this should be merged\n\nThe nix flake is still required by the AvalancheGo `polyrepo` build\ntool. Removing it breaks polyrepo which relies on the flake to build\nFirewood and set up the testing infrastructure used for scheduled daily\nbenchmarks, experiments, and A/B testing.\n\nThis revert restores that functionality until the migration to\nAvalancheGo is complete and the flake can be safely removed as part of\nthat transition.\n\n## How this works\n\nThis reverts commit 76a2c8c17e640604ce82d11b5a2779c9dbdd7c04.\n\n## How this was tested\n```\nTEST=firewood-101-250k FIREWOOD_REF=es/revert-remove-nix-flake just bench-cchain\n```\n- Proves just bench-cchain works\n- Successful build at FIREWOOD_REF\nhttps://github.com/ava-labs/avalanchego/actions/runs/23064572050/job/66999562936#step:5:257\n\n## Breaking Changes\n\n- [ ] firewood\n- [ ] firewood-storage\n- [ ] firewood-ffi (C api)\n- [ ] firewood-go (Go api)\n- [ ] fwdctl\n\n---------\n\nCo-authored-by: maru <maru.newby@avalabs.org>",
+          "timestamp": "2026-03-14T12:43:41Z",
+          "url": "https://github.com/ava-labs/firewood/commit/5406b6858e002d783b5a9c32db9b49fd18291c0b"
+        },
+        "date": 1773647390824,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[40000001,41000000]-Config-firewood-Runner-avago-runner-i4i-2xlarge-local-ssd - mgas/s",
+            "value": 145.74713159162602,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[40000001,41000000]-Config-firewood-Runner-avago-runner-i4i-2xlarge-local-ssd - ms/ggas",
+            "value": 6861.198495500653,
+            "unit": "ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[40000001,41000000]-Config-firewood-Runner-avago-runner-i4i-2xlarge-local-ssd - block_parse_ms/ggas",
+            "value": 110.13825570971645,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[40000001,41000000]-Config-firewood-Runner-avago-runner-i4i-2xlarge-local-ssd - block_verify_ms/ggas",
+            "value": 6672.781373033463,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[40000001,41000000]-Config-firewood-Runner-avago-runner-i4i-2xlarge-local-ssd - block_accept_ms/ggas",
+            "value": 75.80448992550646,
             "unit": "block_accept_ms/ggas"
           }
         ]
