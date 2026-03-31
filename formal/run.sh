06be254fd755112714aaa06091ad8423fa1b8c02
Bernard Wong <bernard@avalabs.org>
2026-03-31 18:06:55 -0400

    Removed formal verification from this branch

--- formal/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-JAVA=/opt/homebrew/opt/openjdk/bin/java
-JAR="$(dirname "$0")/tla2tools.jar"
-SPEC="${1:-ChangeProofVerification}"
-
-cd "$(dirname "$0")"
-exec "$JAVA" -XX:+UseParallelGC -jar "$JAR" -config "${SPEC}.cfg" "${SPEC}.tla"
